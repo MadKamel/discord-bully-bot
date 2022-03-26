60754fda5eef84f846e612d5dad6165ba038e4c7
MadKamel <rustrumek@gmail.com>
2022-03-26 16:57:32 -0700

    Changed up the thing.

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ async def on_ready():
 @client.event
 async def on_message(message):
     print('A user speaks: "' + message.content + '".')
+    split_msg = message.content.split(" ")
     if message.author == client.user:
         return
-    if message.content == 'e' or message.content == 'E':
-        print('Message was "e" or "E"- send a gif.')
+    if 'e' in split_msg or 'E' in split_msg:
+        print('Message had "e" or "E"- send a gif.')
         await message.channel.send("https://tenor.com/view/laugh-neptune-neptunia-nep-gif-7234123")
+    if message.content.lower() == 'lol':
+        print('Message was a permutation of "lol"- send a gif.')
+        await message.channel.send("https://tenor.com/view/neptunia-laughing-neptune-lol-lmao-gif-10075190")
 
 client.run(token)

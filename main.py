d399c6b9c7062e352b6be385149771d29d0e2c5e
MadKamel <rustrumek@gmail.com>
2022-03-26 19:32:51 -0700

    Tried to incorporate bullying function.

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ token = tokenfile.read()
 
 client = discord.Client()
 
+async def push_around(poor_user, channel_1, channel_2):
+    while True:
+            await poor_user.move_to(channel_1)
+            await poor_user.move_to(channel_2)
+
 @client.event
 async def on_ready():
 	print(f'{client.user} is ready.')
@@ -18,6 +23,12 @@ async def on_message(message):
     split_msg = message.content.split(" ")
     if message.author == client.user:
         return
+    if 'bully' in split_msg:
+        print('Message had "bully"')
+        poor_user = client.get_user(800456043884183563) #victim
+        channel_1 = client.get_channel(902706256417005588)
+        channel_2 = client.get_channel(666313740672565277)
+        await push_around(poor_user, channel_1, channel_2)
     if 'e' in split_msg or 'E' in split_msg:
         print('Message had "e" or "E"- send a gif.')
         await message.channel.send("https://tenor.com/view/laugh-neptune-neptunia-nep-gif-7234123")
